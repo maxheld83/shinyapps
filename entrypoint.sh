214630c0b846127ee8cb80a3199f13d6804b3b95
Maximilian Held <info@maxheld.de>
2019-03-19 17:50:20 +0100

    test installing more pkgs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
+Rscript -e "install.packages(c('glue'), repos = 'https://cloud.r-project.org/')"
 Rscript -e "rsconnect::setAccountInfo(name='maxheld83', token='${SHINYAPPS_TOKEN}', secret='${SHINYAPPS_SECRET}')"
 sh -c "Rscript $*"
